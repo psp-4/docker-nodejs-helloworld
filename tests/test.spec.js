40551d02834df613e44e28062538593b22565f23
Parthasarathi Pradhan <cod.psp4@gmail.com>
2023-11-29 19:52:59 +0530

    Update test.spec.js

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 
 test('API-Testing', async ({ page }) => {
     await page.goto('http://localhost:3000/');
-    const greeting = await page.textContent('body');
+    const greeting = await page.innerText('body');
     expect(greeting).toEqual('Hello World');
 });
