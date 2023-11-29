7ae2bcda305bf361f9678428b98cf85e02f0020f
Parthasarathi Pradhan <cod.psp4@gmail.com>
2023-11-29 20:09:20 +0530

    Update test.spec.js

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -5,4 +5,4 @@ test('API-Testing', async ({ page }) => {
     await page.goto('http://localhost:3000/');
     const greeting = await page.innerText('body');
     expect(greeting).toEqual('Hello World');
-});
+}).skip(browserName==='webkit');
